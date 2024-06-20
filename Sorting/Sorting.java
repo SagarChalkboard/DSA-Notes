4ec78ffed164636b9b25bb264190244758b034d0
sagar6 <94176118+SagarChalkboard@users.noreply.github.com>
2024-06-20 20:03:16 +0000

    Changed

--- Sorting/Sorting.java
+++ Sorting/Sorting.java
@@ -12,6 +12,22 @@ public class Sorting {
         }
     }
 
+    public static void selectionSort(int arr[]) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+    }
+
+
+
     public static void printArray(int arr[]) {
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
